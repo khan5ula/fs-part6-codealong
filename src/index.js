622489e407f4c8906f196880ad2764791b10137d
Kristian Hannula <kristian.hannula@proton.me>
2023-06-28 18:35:55 +0300

    followed codealong till 6.14

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
-
-import noteReducer from './reducers/noteReducer'
+import noteService from './services/notes'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
-
 
 const store = configureStore({
   reducer: {
