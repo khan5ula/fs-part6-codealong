6d6a01e49830b9cb25bf26448da4a43567b1d01e
Kristian Hannula <kristian.hannula@proton.me>
2023-06-29 14:25:08 +0300

    followed codealong till 6.16

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 import App from './App'
-import noteService from './services/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
