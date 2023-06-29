a0a2619b2fe6ad1af6fa1b2ce666d839333f7f78
Kristian Hannula <kristian.hannula@proton.me>
2023-06-29 20:36:07 +0300

    initialized new branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { useEffect } from 'react'
-import NewNote from './components/NewNote'
-import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter'
-
-import { useDispatch } from 'react-redux'
-import { initializeNotes } from './reducers/noteReducer'
-
 const App = () => {
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    console.log(content)
+  }
 
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(initializeNotes())
-  }, [dispatch])
+  const toggleImportance = (note) => {
+    console.log('toggle importance of', note.id)
+  }
 
-  return (
+  const notes = []
+
+  return(
     <div>
-      <NewNote />
-      <VisibilityFilter />
-      <Notes />
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map(note =>
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content} 
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      )}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store'
+
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <App />
 )
